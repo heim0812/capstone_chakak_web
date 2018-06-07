f0158046a97ef396e0cb8385cdc2f896be535359
INNOD-NOTEBOOK\innoD <heim0812@gmail.com>
2018-06-07 19:06:00 +0900

    fileChecker, onClick

---
+++ fileCheck.js
@@ -0,0 +1,25 @@
+function fileChecker(name) {
+	var temp_pre = "";
+	var temp_post = "";
+	for (int i = 0; i < 4; i++) {
+		temp_pre = temp_pre + name[i];
+	}
+	for (int j = 0; j < 3; j++) {
+		temp_post = name[name.length - 1 - j] + temp_post;
+	}
+	if (temp_pre == "2018") {
+		return 0;
+	}
+	else if (temp_post == "png") {
+		return 0;
+	}
+	else if (temp_pre == "2017") {
+		return 2;
+	}
+	else if (temp_post == "jpg") {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
